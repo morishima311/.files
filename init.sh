0bfb66394993f720875190ed97fc18f9a391f718
morishima311 <ryoryo311@gmail.com>
2018-06-11 22:40:49 +0900

    download molokai and cp to .vim/colors

--- init.sh
+++ init.sh
@@ -5,5 +5,10 @@
 # Directory that this script is in
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+# download molokai
+git clone https://github.com/tomasr/molokai
+mkdir -p ~/.vim/colors
+cp ./molokai/colors/molokai.vim ~/.vim/colors/
+
 # Set symbolic link of .vimrc to home directory
 ln -s $SCRIPT_DIR/.vimrc $HOME/.vimrc
