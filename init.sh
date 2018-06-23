d14ebd0682642eedd92533da0227371490210666
morishima311 <morishima@west.sd.keio.ac.jp>
2018-06-23 10:41:08 +0900

    add .tmux.conf and set init.sh

--- init.sh
+++ init.sh
@@ -5,17 +5,16 @@
 # Directory that this script is in
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-echo "Download molokai"
+echo "----Download molokai----"
 git clone https://github.com/tomasr/molokai
 
-# error occur
-if [ $? != 0 ]; then exit; fi
-
-echo "copy molokai to ~/.vim/colors"
+echo "----copy molokai to ~/.vim/colors----"
 mkdir -p ~/.vim/colors
 cp ./molokai/colors/molokai.vim ~/.vim/colors/
 
-echo "make symbolic link of .vimrc to ~"
-ln -s $SCRIPT_DIR/.vimrc $HOME/.vimrc
+echo "----make symbolic link of .vimrc to HOME----"
+ln -sfi $SCRIPT_DIR/.vimrc $HOME/.vimrc
+echo "----make symbolic link of .tmux.conf to HOME----"
+ln -sfi $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
 
-echo "MY WORK IS DONE!"
+echo "----MY WORK IS DONE!----"
