5aab3006da98b271efa54a79a81c24f991c7615d
morishima311 <morishima@west.sd.keio.ac.jp>
2018-10-05 11:41:06 +0900

    add ssh agent and tmux config on bashrc

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#This script initialize vimrc setting
-
-# Directory that this script is in
+# Directory which this script is in
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 echo "----Download molokai----"
@@ -16,5 +14,21 @@ echo "----make symbolic link of .vimrc to HOME----"
 ln -sfi $SCRIPT_DIR/.vimrc $HOME/.vimrc
 echo "----make symbolic link of .tmux.conf to HOME----"
 ln -sfi $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf
+
+echo "----ssh agent and tmux configuration----"
+echo "
+# renew SSH_AUTH_SOCK when attach tmux. added by \".files\"
+agent=\"$HOME/.ssh/agent\"
+if [ -S \"$SSH_AUTH_SOCK\" ]; then
+    case $SSH_AUTH_SOCK in
+    /tmp/*/agent.[0-9]*)
+        ln -snf \"$SSH_AUTH_SOCK\" $agent && export SSH_AUTH_SOCK=$agent
+    esac
+elif [ -S $agent ]; then
+    export SSH_AUTH_SOCK=$agent
+else
+    echo \"no ssh-agent\"
+fi
+" >> $HOME/.bashrc
 
 echo "----MY WORK IS DONE!----"
